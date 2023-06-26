5992e04b642d01f17f6e971fe911e23198fecf76
Pedro Ferreira <pedrolcsferreira@gmail.com>
2023-06-26 17:22:18 -0300

    chore:level 3 - tests

--- api/src/modules/payables/payable.controller.spec.ts
+++ api/src/modules/payables/payable.controller.spec.ts
@@ -3,7 +3,7 @@ import { PayableController } from './payable.controller';
 import { PayableService } from './payable.service';
 import { NotFoundException, BadRequestException } from '@nestjs/common';
 import { PrismaService } from '../../prisma.service';
-
+import { format } from 'date-fns';
 describe('PayableController', () => {
   let controller: PayableController;
   let service: PayableService;
@@ -69,5 +69,75 @@ describe('PayableController', () => {
     );
 
     expect(result).toEqual({ payable });
+  });
+
+  it('should able to list a array payables', async () => {
+    const payable = {
+      id: '33f48f73-be98-487b-8558-5e72ca6df787',
+      value: 80,
+      emissionDate: new Date(),
+      emissionDateFiltered: format(new Date(), 'dd/MM/yyyy'),
+      assignorId: '12414ddb-bf3e-4bb3-9f93-3f7ba7084dfa',
+    };
+
+    jest.spyOn(service, 'list').mockResolvedValue({
+      count: 1,
+      payables: [
+        {
+          ...payable,
+        },
+      ],
+    });
+
+    const result = await controller.list(1, 10);
+
+    expect(result).toEqual({
+      count: 1,
+      payables: [
+        {
+          ...payable,
+        },
+      ],
+    });
+  });
+
+  it('should able to update a payable', async () => {
+    const payableDto = {
+      id: '33f48f73-be98-487b-8558-5e72ca6df787',
+      value: 80,
+      emissionDate: new Date(),
+      emissionDateFiltered: format(new Date(), 'dd/MM/yyyy'),
+      assignorId: '12414ddb-bf3e-4bb3-9f93-3f7ba7084dfa',
+    };
+
+    const createdPayable = {
+      ...payableDto,
+    };
+    jest
+      .spyOn(service, 'update')
+      .mockResolvedValue({ payable: createdPayable });
+
+    const result = await controller.updatePayable(payableDto.id, payableDto);
+
+    expect(result).toEqual({ payable: createdPayable });
+  });
+
+  it('should able to delete a payable', async () => {
+    const payableDto = {
+      id: '33f48f73-be98-487b-8558-5e72ca6df787',
+      value: 80,
+      emissionDate: new Date(),
+      emissionDateFiltered: format(new Date(), 'dd/MM/yyyy'),
+      assignorId: '12414ddb-bf3e-4bb3-9f93-3f7ba7084dfa',
+    };
+
+    const createdPayable = {
+      ...payableDto,
+    };
+    jest.spyOn(service, 'delete').mockResolvedValue(createdPayable);
+
+    const result = await controller.delete(payableDto.id);
+
+    expect(result).toEqual(createdPayable);
   });
 });
